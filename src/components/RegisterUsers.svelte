957c4c4e4952f16220641e2d0c7d0b001fa1a445
MikeFranco <mfranco_98@yahoo.com>
2020-07-09 15:17:26 -0500

    added create user connection

--- src/components/RegisterUsers.svelte
+++ src/components/RegisterUsers.svelte
@@ -1,3 +1,44 @@
+<script>
+  import axios from "axios";
+  let firstName;
+  let secondName;
+  let firstLastName;
+  let secondLastName;
+  let email;
+  let phone;
+  let district;
+  let purchased;
+  const createUser = event => {
+    const adminId = "5f065d34b89f6c5b54174eca";
+    const user = {
+      firstName,
+      secondName,
+      firstLastName,
+      secondLastName,
+      email,
+      phone,
+      district,
+      purchased,
+      adminId
+    };
+    const body = { user };
+    if (firstName && firstLastName && email && phone) {
+      axios
+        .post("http://localhost:5001/users", body)
+        .then(response => {
+          console.log("%c⧭", "color: #997326", response);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    } else {
+      alert("Favor de llenar los campos requeridos");
+    }
+    event.preventDefault();
+    console.log("%c⧭", "color: #e57373", "submit");
+  };
+</script>
+
 <style>
   #Reg {
     width: 100%;
@@ -21,151 +62,161 @@
   <div class="row">
     <div class="col-md-12">
       <div class="well well-sm">
-        <form class="form-horizontal " method="post">
-          <fieldset>
-            <div>
-              <legend class="text-center header" id="Reg">Create User</legend>
+        <form class="form-horizontal ">
+          <div>
+            <legend class="text-center header" id="Reg">Create User</legend>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-user bigicon" />
+            </span>
+            <div class="col-md-8">
+              <input
+                bind:value={firstName}
+                id="fname"
+                name="name"
+                type="text"
+                placeholder="First Name"
+                class="form-control" />
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-user bigicon" />
-              </span>
-              <div class="col-md-8">
-                <input
-                  id="fname"
-                  name="name"
-                  type="text"
-                  placeholder="First Name"
-                  class="form-control" />
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-user bigicon" />
+            </span>
+            <div class="col-md-8">
+              <input
+                bind:value={secondName}
+                id="secondname"
+                name="name"
+                type="text"
+                placeholder="Second Name"
+                class="form-control" />
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-user bigicon" />
-              </span>
-              <div class="col-md-8">
-                <input
-                  id="secondname"
-                  name="name"
-                  type="text"
-                  placeholder="Second Name"
-                  class="form-control" />
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-user bigicon" />
+            </span>
+            <div class="col-md-8">
+              <input
+                id="firstLastName"
+                bind:value={firstLastName}
+                name="name"
+                type="text"
+                placeholder="First Last Name"
+                class="form-control" />
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-user bigicon" />
-              </span>
-              <div class="col-md-8">
-                <input
-                  id="firstLastName"
-                  name="name"
-                  type="text"
-                  placeholder="First Last Name"
-                  class="form-control" />
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-user bigicon" />
+            </span>
+            <div class="col-md-8">
+              <input
+                bind:value={secondLastName}
+                id="secondLastName"
+                name="name"
+                type="text"
+                placeholder="Second Last Name"
+                class="form-control" />
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-user bigicon" />
-              </span>
-              <div class="col-md-8">
-                <input
-                  id="secondLastName"
-                  name="name"
-                  type="text"
-                  placeholder="Second Last Name"
-                  class="form-control" />
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-envelope-o bigicon" />
+            </span>
+            <div class="col-md-8">
+              <input
+                bind:value={email}
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Email Address"
+                class="form-control" />
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-envelope-o bigicon" />
-              </span>
-              <div class="col-md-8">
-                <input
-                  id="email"
-                  name="email"
-                  type="text"
-                  placeholder="Email Address"
-                  class="form-control" />
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-phone-square bigicon" />
+            </span>
+            <div class="col-md-8">
+              <input
+                bind:value={phone}
+                id="phone"
+                name="phone"
+                type="text"
+                placeholder="Phone"
+                class="form-control" />
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-phone-square bigicon" />
-              </span>
-              <div class="col-md-8">
-                <input
-                  id="phone"
-                  name="phone"
-                  type="text"
-                  placeholder="Phone"
-                  class="form-control" />
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-envelope-o bigicon" />
+            </span>
+            <div class="col-md-8">
+              <p>
+                District:
+                <select name="transporte" id="lista" bind:value={district}>
+                  <option value="">--Select an option--</option>
+                  <option value="Álvaro Obregón">Álvaro Obregón</option>
+                  <option value="Azcapotzalco">Azcapotzalco</option>
+                  <option value="Benito Juárez">Benito Juárez</option>
+                  <option value="Coyoacán">Coyoacán</option>
+                  <option value="Cuajimalpa">Cuajimalpa</option>
+                  <option value="Cuauhtémoc">Cuauhtémoc</option>
+                  <option value="Gustavo A. Madero">Gustavo A. Madero</option>
+                  <option value="Iztacalco">Iztacalco</option>
+                  <option value="Iztapalapa">Iztapalapa</option>
+                  <option value="Magdalena Contreras">
+                    Magdalena Contreras
+                  </option>
+                  <option value="Miguel Hidalgo">Miguel Hidalgo</option>
+                  <option value="Milpa Alta">Milpa Alta</option>
+                  <option value="Tláhuac">Tláhuac</option>
+                  <option value="Tlalpan">Tlalpan</option>
+                  <option value="Venustiano Carranza">
+                    Venustiano Carranza
+                  </option>
+                  <option value="Xochimilco">Xochimilco</option>
+                </select>
+              </p>
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-envelope-o bigicon" />
-              </span>
-              <div class="col-md-8">
-                <p>
-                  District:
-                  <select name="transporte" id="lista">
-                    <option value="">--Select an option--</option>
-                    <option value="Álvaro Obregón">Álvaro Obregón</option>
-                    <option value="Azcapotzalco">Azcapotzalco</option>
-                    <option value="Benito Juárez">Benito Juárez</option>
-                    <option value="Coyoacán">Coyoacán</option>
-                    <option value="Cuajimalpa">Cuajimalpa</option>
-                    <option value="Cuauhtémoc">Cuauhtémoc</option>
-                    <option value="Gustavo A. Madero">Gustavo A. Madero</option>
-                    <option value="Iztacalco">Iztacalco</option>
-                    <option value="Iztapalapa">Iztapalapa</option>
-                    <option value="Magdalena Contreras">
-                      Magdalena Contreras
-                    </option>
-                    <option value="Miguel Hidalgo">Miguel Hidalgo</option>
-                    <option value="Milpa Alta">Milpa Alta</option>
-                    <option value="Tláhuac">Tláhuac</option>
-                    <option value="Tlalpan">Tlalpan</option>
-                    <option value="Venustiano Carranza">
-                      Venustiano Carranza
-                    </option>
-                    <option value="Xochimilco">Xochimilco</option>
-                  </select>
-                </p>
-              </div>
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <span class="col-md-1 col-md-offset-2 text-center">
+              <i class="fa fa-envelope-o bigicon" />
+            </span>
+            <div class="col-md-8">
+              <p>
+                Products:
+                <select name="transporte" id="lista" bind:value={purchased}>
+                  <option value="">--Select an option--</option>
+                  <option value="Cojines">Cojines</option>
+                  <option value="Almohadas">Almohadas</option>
+                  <option value="Canas">Camas</option>
+                  <option value="Colchones">Colchones</option>
+                </select>
+              </p>
             </div>
-            <div class="form-group d-flex justify-content-center">
-              <span class="col-md-1 col-md-offset-2 text-center">
-                <i class="fa fa-envelope-o bigicon" />
-              </span>
-              <div class="col-md-8">
-                <p>
-                  Products:
-                  <select name="transporte" id="lista">
-                    <option value="">--Select an option--</option>
-                    <option value="Cojines">Cojines</option>
-                    <option value="Almohadas">Almohadas</option>
-                    <option value="Canas">Camas</option>
-                    <option value="Colchones">Colchones</option>
-                  </select>
-                </p>
-              </div>
-            </div>
-            <div class="form-group d-flex justify-content-center">
-              <div class="col-md-6 text-center">
-                <a
-                  href="/"
+          </div>
+          <div class="form-group d-flex justify-content-center">
+            <div class="col-md-6 text-center">
+              <button
+                class="btn btn-outline-primary button"
+                style="width:100%;"
+                on:click={createUser}>
+                Submit
+              </button>
+              <!-- <a
+                  href="/User"
                   class="btn btn-outline-primary button"
                   type="button"
                   style="width:100%;">
                   Submit
-                </a>
-              </div>
+                </a> -->
             </div>
-          </fieldset>
+          </div>
         </form>
       </div>
     </div>

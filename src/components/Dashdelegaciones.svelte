596bdb37936a9dbf8c6ddfa194ca02829c5ab916
MikeFranco <mfranco_98@yahoo.com>
2020-07-09 00:22:07 -0500

    added connection for districts chart

--- src/components/Dashdelegaciones.svelte
+++ src/components/Dashdelegaciones.svelte
@@ -1,131 +1,197 @@
 <script>
-import Chart from './charts/Delegacion.svelte';
-let data=[30,19,19,56,78,32,12,34,32,56,78,11,23,154,123,54];
+  import Delegacion from "./charts/Delegacion.svelte";
+  import axios from "axios";
+  import { onMount } from "svelte";
+  let data;
+  let labels;
+  let showChart = false;
+  const getChartInfo = () => {
+    const adminId = "5f065d34b89f6c5b54174eca";
+    axios
+      .get(`http://localhost:5001/users?adminId=${adminId}`)
+      .then(response => {
+        const users = response.data.getUsersByAdminId;
+        const districtData = users.reduce((carry, user, index, self) => {
+          const key = user.district;
+          const keyLength = self.filter(value => value.district == key).length;
+          carry[key] = keyLength;
+          return carry;
+        }, {});
+        labels = Object.keys(districtData);
+        data = Object.values(districtData);
+        showChart = true;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  onMount(getChartInfo);
 </script>
-<br>
-<br>
-<br>
+
+<br />
+<br />
+<br />
 <div class="container">
-		<div class="row justify-content-center">
-			<div id="DashCliente" class="col-12 col-md-12 col-lg-6 d-flex justify-content-center">
-          			<article class="card" style="width: 100%;">
-                		<div class="card-body">
-						 <Chart {data}/>   
-            			</div>
-          			</article>
-        		</div>
-		</div>
-		<div class="row">
-			 	<div id="delegaciones" class="col-12 col-md-4 col-lg-6 d-flex justify-content-center">
-          			<article class="card" style="width: 100%; height: 100%;">
-                		<div class="card-body">
-							<h5 class="card-title"> Usuarios por Alcaldias</h5>
-							   <article class="card" style="width: 100%;height:12%;background: #FFFFFF;">
-                					<div class="card-body">
-										<h5 class="card-title">Álvaro Obregón</h5>
-											<h5>{data[0]}</h5>			
-            						</div>
-          						</article>   
-								<article class="card" style="width: 100%;height:12%;background: #C0C0C0;">
-                					<div class="card-body">
-										<h5 class="card-title">Azcapotzalco</h5>
-											<h5>{data[1]}</h5>			
-            						</div>
-          						</article>  
-								<article class="card" style="width: 100%;height:12%;background: #808080;">
-                					<div class="card-body">
-										<h5 class="card-title">Benito Juárez</h5>
-											<h5>{data[2]}</h5>			
-            						</div>
-          						</article> 
-								  <article class="card" style="color:white; width: 100%;height:12%;background: #000000;">
-                					<div class="card-body">
-										<h5 class="card-title">Coyoacán</h5>
-											<h5>{data[3]}</h5>			
-            						</div>
-          						</article>
-								<article class="card" style="color:white; width: 100%;height:12%;background: #FF0000;">
-                					<div class="card-body">
-										<h5 class="card-title">Cuajimalpa de Morelos</h5>
-											<h5>{data[4]}</h5>			
-            						</div>
-          						</article> 
-								<article class="card" style=" color:white; width: 100%;height:12%;background: #800000;">
-                					<div class="card-body">
-										<h5 class="card-title">Cuauhtémoc</h5>
-											<h5>{data[5]}</h5>			
-            						</div>
-          						</article> 
-								<article class="card" style=" width: 100%;height:12%;background: #FFFF00;">
-                					<div class="card-body">
-										<h5 class="card-title">Gustavo A. Madero</h5>
-											<h5>{data[6]}</h5>			
-            						</div>
-          						</article> 
-								<article class="card" style="width: 100%;height:12%;background: #808000;">
-                					<div class="card-body">
-										<h5 class="card-title">Iztacalco</h5>
-											<h5>{data[7]}</h5>			
-            						</div>
-          						</article>     				
-            			</div>
-          			</article>
-        		</div>	
-            <div id="delegaciones" class="col-12 col-md-4 col-lg-6 d-flex justify-content-center">
-                <article class="card" style="width: 100%; height: 100%;">
-                    <div class="card-body">
-              <h5 class="card-title">Usuarios por Alcaldias</h5>
-                    <article class="card" style="width: 100%;height:12%;background: #00FF00 ;">
-                              <div class="card-body">
-                        <h5 class="card-title">Iztapalapa</h5>
-                          <h5>{data[8]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="color:white; width: 100%;height:12%;background: #008000;">
-                              <div class="card-body">
-                        <h5 class="card-title">Magdalena Contreras</h5>
-                          <h5>{data[9]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="width: 100%;height:12%;background: #00FFFF;">
-                              <div class="card-body">
-                        <h5 class="card-title">Miguel Hidalgo</h5>
-                          <h5>{data[10]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="color:white; width: 100%;height:12%;background: #008080;">
-                              <div class="card-body">
-                        <h5 class="card-title">Milpa Alta</h5>
-                          <h5>{data[11]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="color:white;  width: 100%;height:12%;background: #0000FF;">
-                              <div class="card-body">
-                        <h5 class="card-title">Tláhuac</h5>
-                          <h5>{data[12]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="color:white;  width: 100%;height:12%;background: #000080;">
-                              <div class="card-body">
-                        <h5 class="card-title">Tlalpan</h5>
-                          <h5>{data[13]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="width: 100%;height:12%;background: #FF00FF;">
-                              <div class="card-body">
-                        <h5 class="card-title">Venustiano Carranza</h5>
-                          <h5>{data[14]}</h5>     
-                            </div>
-                    </article> 
-                    <article class="card" style="color:white;  width: 100%;height:12%;background: #800080 ;">
-                              <div class="card-body">
-                        <h5 class="card-title">Xochimilco</h5>
-                          <h5>{data[15]}</h5>     
-                            </div>
-                    </article>              
-                  </div>
-                </article>
-            </div>  
-			 	
-		</div>
-	</div>
+  <div class="row justify-content-center">
+    <div
+      id="DashCliente"
+      class="col-12 col-md-12 col-lg-6 d-flex justify-content-center">
+      <article class="card" style="width: 100%;">
+        <div class="card-body">
+          {#if showChart}
+            <Delegacion {data} {labels} />
+          {/if}
+        </div>
+      </article>
+    </div>
+  </div>
+  <!--  <div class="row">
+    <div
+      id="delegaciones"
+      class="col-12 col-md-4 col-lg-6 d-flex justify-content-center">
+      <article class="card" style="width: 100%; height: 100%;">
+        <div class="card-body">
+          <h5 class="card-title">Usuarios por Alcaldias</h5>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #FFFFFF;">
+            <div class="card-body">
+              <h5 class="card-title">Álvaro Obregón</h5>
+              <h5>{data[0]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #C0C0C0;">
+            <div class="card-body">
+              <h5 class="card-title">Azcapotzalco</h5>
+              <h5>{data[1]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #808080;">
+            <div class="card-body">
+              <h5 class="card-title">Benito Juárez</h5>
+              <h5>{data[2]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #000000;">
+            <div class="card-body">
+              <h5 class="card-title">Coyoacán</h5>
+              <h5>{data[3]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #FF0000;">
+            <div class="card-body">
+              <h5 class="card-title">Cuajimalpa de Morelos</h5>
+              <h5>{data[4]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style=" color:white; width: 100%;height:12%;background: #800000;">
+            <div class="card-body">
+              <h5 class="card-title">Cuauhtémoc</h5>
+              <h5>{data[5]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style=" width: 100%;height:12%;background: #FFFF00;">
+            <div class="card-body">
+              <h5 class="card-title">Gustavo A. Madero</h5>
+              <h5>{data[6]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #808000;">
+            <div class="card-body">
+              <h5 class="card-title">Iztacalco</h5>
+              <h5>{data[7]}</h5>
+            </div>
+          </article>
+        </div>
+      </article>
+    </div>
+    <div
+      id="delegaciones"
+      class="col-12 col-md-4 col-lg-6 d-flex justify-content-center">
+      <article class="card" style="width: 100%; height: 100%;">
+        <div class="card-body">
+          <h5 class="card-title">Usuarios por Alcaldias</h5>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #00FF00 ;">
+            <div class="card-body">
+              <h5 class="card-title">Iztapalapa</h5>
+              <h5>{data[8]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #008000;">
+            <div class="card-body">
+              <h5 class="card-title">Magdalena Contreras</h5>
+              <h5>{data[9]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #00FFFF;">
+            <div class="card-body">
+              <h5 class="card-title">Miguel Hidalgo</h5>
+              <h5>{data[10]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #008080;">
+            <div class="card-body">
+              <h5 class="card-title">Milpa Alta</h5>
+              <h5>{data[11]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #0000FF;">
+            <div class="card-body">
+              <h5 class="card-title">Tláhuac</h5>
+              <h5>{data[12]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #000080;">
+            <div class="card-body">
+              <h5 class="card-title">Tlalpan</h5>
+              <h5>{data[13]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="width: 100%;height:12%;background: #FF00FF;">
+            <div class="card-body">
+              <h5 class="card-title">Venustiano Carranza</h5>
+              <h5>{data[14]}</h5>
+            </div>
+          </article>
+          <article
+            class="card"
+            style="color:white; width: 100%;height:12%;background: #800080 ;">
+            <div class="card-body">
+              <h5 class="card-title">Xochimilco</h5>
+              <h5>{data[15]}</h5>
+            </div>
+          </article>
+        </div>
+      </article>
+    </div>
+
+  </div> -->
+</div>
--- src/components/charts/Delegacion.svelte
+++ src/components/charts/Delegacion.svelte
@@ -1,34 +1,33 @@
-<canvas id="myChart" width="100" height="100" />
 <script>
   import { onMount } from "svelte";
   export let data;
+  export let labels;
+
   const createChart = () => {
     var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {
       type: "pie",
       data: {
-       
         datasets: [
           {
             data,
-
             backgroundColor: [
-              "rgba(255, 255, 255, 0.5)",
-              "rgba(192, 192, 192, 0.5)",
-              "rgba(128, 128, 128, 0.5",
-              "rgba(0, 0, 0, 0.5)",
-              "rgba(255, 0, 0, 0.5)",
-              "rgba(128, 0, 0, 0.5)",
-              "rgba(255, 255, 0, 0.5)",
-              "rgba(128, 128, 0, 0.5)",
-              "rgba(0, 255, 0, 0.5)",
-              "rgba(0, 128, 0, 0.5)",
-              "rgba(0, 255, 255, 0.5)",
-              "rgba(0, 128, 128, 0.5)",
-              "rgba(0, 0, 255, 0.5)",
-              "rgba(0, 0, 128, 0.5)",
-              "rgba(255, 0, 255, 0.5)",
-              "rgba(128, 0, 128, 0.5)"
+              "rgba(255, 0, 0, 0.8)",
+              "rgba(128, 0, 0, 0.8)",
+              "rgba(255, 255, 0, 0.8)",
+              "rgba(128, 128, 0, 0.8)",
+              "rgba(0, 255, 0, 0.8)",
+              "rgba(0, 128, 0, 0.8)",
+              "rgba(0, 255, 255, 0.8)",
+              "rgba(0, 128, 128, 0.8)",
+              "rgba(0, 0, 255, 0.8)",
+              "rgba(0, 0, 128, 0.8)",
+              "rgba(255, 0, 255, 0.8)",
+              "rgba(128, 0, 128, 0.8)",
+              "rgba(255, 255, 255, 0.8)",
+              "rgba(192, 192, 192, 0.8)",
+              "rgba(128, 128, 128, 0.8",
+              "rgba(0, 0, 0, 0.8)",
             ],
             borderColor: [
               "rgb(255, 255, 255)",
@@ -50,20 +49,21 @@
             ],
             borderWidth: 1
           }
-        ]
+        ],
+        labels: labels
       },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
+      option: {
+        responsive: true,
+        legend: {
+          display: false,
+          position: 'bottom',
+          reverse: true
+        },
+        rotation: 0.5 * Math.PI
       }
     });
   };
   onMount(createChart);
 </script>
+
+<canvas id="myChart" width="100" height="100" />
